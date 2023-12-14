58866433c27c649e466d7b7dceb519fd22f3110b
CubeRuben <33936898+CubeRuben@users.noreply.github.com>
2023-12-14 23:08:59 +0300

    Models changes

--- ieclasterrization/main/models.py
+++ ieclasterrization/main/models.py
@@ -3,5 +3,12 @@ from django.db import models
 class Region(models.Model):
     name = models.CharField(max_length=256)
 
+    def __str__(self):
+        return self.name
+
 class Industry(models.Model):
     name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+ 
